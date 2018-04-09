4e66600c82e88e2ae65fdc53d5b09edce85d714a
Mario Mendoza <mario.amg03@gmail.com>
2018-04-09 10:58:51 -0400

    Uso del monitor serial
    En esta clase vamos a hablar sobre la forma en la que Arduino se comunica con la computadora.

--- Curso_Arduino/Curso_Arduino.ino
+++ Curso_Arduino/Curso_Arduino.ino
@@ -5,10 +5,12 @@ int potValue = 0;                         //declaramos una variable para guardar
 
 void setup() {
   pinMode(led, OUTPUT);                   //declaramos el led como salida
+  Serial.begin(9600);                     //inicializacion del monitor serial de arduino
 }
 
 void loop() {
   potValue = analogRead(potPin);          //almacenamos el valor del pin analogico de nuestro potenciometro
+  Serial.println(potValue);               //imprimo el valor de potValue en el monitor serial
   digitalWrite(led, HIGH);                //encendemos el led
   delay(potValue);                        //damos un delay igual al valor recibido por nuestro potenciometro
   digitalWrite(led, LOW);                 //apagamos el led
