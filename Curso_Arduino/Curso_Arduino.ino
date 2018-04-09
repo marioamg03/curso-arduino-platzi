20f487c020ed359e54c75df5397cc93666f569be
Mario Mendoza <mario.amg03@gmail.com>
2018-04-09 10:05:56 -0400

    Declarando variables y Constantes
    Usar variables y constantes globales para definir nuestros pines es una buena practica en el desarrollo de hardwarware. estas se realizan antes del setup.

--- Curso_Arduino/Curso_Arduino.ino
+++ Curso_Arduino/Curso_Arduino.ino
@@ -1,13 +1,20 @@
+//Variable global
+int led1 = 2; //donde  int indica que es una variable de tipo entero, led1 es el nombre de la variable y 2 el valor
+
+//Constante global 
+const byte led2 = 3; //de esta forma declaramos constantes
+
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2,OUTPUT);
+  pinMode(led1,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(2,HIGH);
+  digitalWrite(led1,HIGH);
   delay(100);
-  digitalWrite(2,LOW);
+  digitalWrite(led1,LOW);
   delay(100);
   
 }
